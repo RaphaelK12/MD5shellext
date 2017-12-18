0d00c774ac36f472a0b77247a225edca84a69ea3
Andrey Belyaev <svetomech@gmail.com>
2017-12-18 16:07:01 +0300

    Update repo link

--- MD5shellext/Properties/AssemblyInfo.cs
+++ MD5shellext/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MD5shellext")]
-[assembly: AssemblyDescription("https://github.com/Svetomech/md5shellext")]
+[assembly: AssemblyDescription("https://github.com/Svetomech/MD5shellext")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Svetomech")]
 [assembly: AssemblyProduct("MD5shellext")]
